1eee9012fae445ff3514e2588401e3758d4b16ba
FRANK WOLFF HANNEMANN <fwh22@inf.ufpr.br>
2023-09-20 19:11:26 -0300

    [ASS] trabalho quase pronto

--- 4_periodo/icc/ep03/interpola.c
+++ 4_periodo/icc/ep03/interpola.c
@@ -30,36 +30,102 @@ void imprimirVetorPontos(Ponto *vetor, uint n) {
         }
 }
 
-double interpolacaoLagrange(Ponto *vetor, uint n, uint x) {
-
+double interpolacaoLagrange(Ponto *vetor, uint n, double x) {
+        double den = 1, xi, li, resultado;
         double num = x - vetor[0].x;
 
         for (uint i = 1; i < n; i++) {
                 num *= (x - vetor[i].x);
         }
+        //printf("num: %lf", num);
 
-        double den;
-
+        resultado = 0;
         for (uint i = 0; i < n; i++) {
+                den = 1;
+                xi = vetor[i].x;
                 for (uint j = 0; j < n; j++) {
+                        if (i != j) {
+                                den *= (xi - vetor[j].x);
+                        }
                 }
+                den *= x - xi;
+                //printf("den: %lf", den);
+                li = num / den;
+                resultado += li * vetor[i].y;
+                //printf("%lf\n", resultado);
         }
-
+        return resultado;
 }
 
-int main() {
+double calculaDiferencaDividida(Ponto *vetor, uint a, uint b) {
+
+        if (a == b) {
+                return vetor[a].y;
+        }
+
+        double mult = (calculaDiferencaDividida(vetor, a + 1, b) - calculaDiferencaDividida(vetor, a, b - 1)) / (vetor[b].x - vetor[a].x);
+
+        return mult;
+}
+
+double interpolacaoNewton(Ponto *vetor, uint n, double x) {
+
+        double resultado = 0;
+
+        for (uint i = 0; i < n; i++) {
+                double tmp = calculaDiferencaDividida(vetor, 0, i);
+                for (uint j = 0; j < i; j++) {
+                        tmp *= x - vetor[j].x;
+                }
+                resultado += tmp;
+        }
+
+        return resultado;
+}
+
+int main(int argc, char **argv) {
+        LIKWID_MARKER_INIT;
+
+
+        if (argc < 1)
+                return 1;
 
         uint n;
+        double x = atof(argv[1]);
         scanf("%u", &n);
 
         Ponto *entrada = criarVetorPontos(n);
         for(uint i = 0; i < n; i++) {
                 scanf("%lf %lf", &entrada[i].x, &entrada[i].y);
         }
+        
+        double tempoL = timestamp();
 
-        imprimirVetorPontos(entrada, n);
+         LIKWID_MARKER_START("interpolacaoLagrange");
 
+        double respostaLagrange = interpolacaoLagrange(entrada, n, x);
+
+        tempoL = timestamp() - tempoL;
+
+        LIKWID_MARKER_STOP("interpolacaoLagrange");
+
+        double tempoN = timestamp();
+
+        LIKWID_MARKER_START("interpolacaoNewton");
+
+        double respostaNewton = interpolacaoNewton(entrada, n, x);
+
+        tempoN = timestamp() - tempoN;
+
+        LIKWID_MARKER_STOP("interpolacaoNewton");
+
+        printf("P%u(%lf) = %.8lf\n", n, x, respostaLagrange);
+        printf("P%u(%lf) = %.8lf\n", n, x, respostaNewton);
+        printf("Tempo Lagrange: %lf\n", tempoL);
+        printf("Tempo Newton: %lf\n", tempoN);
+        
         destruirVetorPontos(entrada);
 
+        LIKWID_MARKER_CLOSE;
         return 0;
 }
---
+++ 4_periodo/icc/ep03/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+METRICA="FLOPS_DP"
+CPU=3
+
+LIKWID_HOME=/home/soft/likwid
+CFLAGS="-I${LIKWID_HOME}/include -DLIKWID_PERFMON"
+LFLAGS="-L${LIKWID_HOME}/lib -llikwid"
+
+echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
+for k in $METRICA
+do
+    likwid-perfctr -C ${CPU} -g ${k} -m ./interpola >${k}.log
+done
+
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
