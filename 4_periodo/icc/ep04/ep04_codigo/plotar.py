2f23ee4ff7b3cf428733f5248d1855f6b6f321fa
Caio H. R. R. <caiohrr@gmail.com>
2023-10-27 11:55:25 -0300

    [FIX] Removendo diretório inútil

--- 4_periodo/icc/ep04/ep04_codigo/plotar.py
+++ 4_periodo/icc/ep04/ep04_codigo/plotar.py
@@ -1,16 +1,18 @@
 import re
 import matplotlib.pyplot as plt
+from matplotlib import style
 
 #x_values = [64, 100, 128, 200, 256, 512, 600, 900, 1024, 2000, 2048, 3000, 4000]
 x_values = [64, 100, 128, 200]
 
 entry_values = []
 
-with open("FLOPS_DP.dat") as file:
+with open("L2CACHE.dat") as file:
     for item in file:
-        match = re.findall(r"[-+]?(?:\d*\.*\d+)", item)
-        if match:
-            entry_values.append(match[0]) 
+        entry_values.append(float(item.split("|")[2]))
+        #match = re.findall(r"[-+]?(?:\d*\.*\d+)", item)
+        #if match:
+        #    entry_values.append(match[0]) 
 
 matVetY = []
 matVetOtimizadoY= []
@@ -27,12 +29,29 @@ while count < 4 * num_of_tests:
     matMatOtimizadoY.append(entry_values[count + 3])
     count += 4
 
-plt.subplots(2, 1)
+print(matVetY)
+print(matVetOtimizadoY)
+print(matMatY)
+print(matMatOtimizadoY)
 
-plt.plot(x_values, matVetY, color='r', label='matVetY')
-plt.plot(x_values, matVetOtimizadoY, color='b', label='matVetOtimizadoY')
-plt.title('matVet')
-plt.legend(loc='lower right')
-plt.ylabel('MFLOP/s')
-plt.xlabel('N')
+plt.style.use('ggplot')
+
+fig, (matVet, matMat) = plt.subplots(2, 1)
+
+matVet.plot(x_values, matVetY, label='matVetY')
+matVet.plot(x_values, matVetOtimizadoY, label='matVetOtimizadoY')
+
+matMat.plot(x_values, matMatY, label='matMatY')
+matMat.plot(x_values, matMatOtimizadoY, label='matMatOtimizadoY')
+
+matVet.legend(loc='lower right')
+matVet.set_title('FLOPS_DP, Matriz-vetor')
+matVet.set_ylabel('MFLOP/s')
+matVet.set_xlabel('N')
+
+matMat.legend(loc='lower right')
+matMat.set_title('FLOPS_DP, Matriz-matriz')
+matMat.set_ylabel('MFLOP/s')
+matMat.set_xlabel('N')
+
 plt.show()
